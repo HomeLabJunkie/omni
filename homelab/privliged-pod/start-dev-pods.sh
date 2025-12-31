258a65e906aa032b0e5f19e0069005334023cb5e
Jeff Riffle <jeffriffle@gmail.com>
2025-12-31 13:53:06 -0600

    update debug pod scripts

--- homelab/privliged-pod/start-dev-pods.sh
+++ homelab/privliged-pod/start-dev-pods.sh
@@ -1,3 +1,3 @@
 ##!/bin/bash
 
-kubectl apply -f debug-node-0.yaml -f debug-node-1.yaml -f debug-node-2.yaml -f debug-node-3.yaml -f debug-node-4.yaml -f debug-node-5.yaml
+kubectl apply -f debug-node-1.yaml -f debug-node-2.yaml -f debug-node-3.yaml -f debug-node-4.yaml -f debug-node-5.yaml
--- homelab/privliged-pod/stop-dev-pods.sh
+++ homelab/privliged-pod/stop-dev-pods.sh
@@ -1,3 +1,3 @@
 ##!/bin/bash
 
-kubectl delete -f debug-node-0.yaml -f debug-node-1.yaml -f debug-node-2.yaml -f debug-node-3.yaml -f debug-node-4.yaml -f debug-node-5.yaml
+kubectl delete -f debug-node-1.yaml -f debug-node-2.yaml -f debug-node-3.yaml -f debug-node-4.yaml -f debug-node-5.yaml
